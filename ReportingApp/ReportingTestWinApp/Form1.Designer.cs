052683426d7a7db9e3580645c08d336a8189b35c
niranjankala <niranjankala@aol.com>
2017-09-15 11:58:21 +0530

    -

--- ReportingApp/ReportingTestWinApp/Form1.Designer.cs
+++ ReportingApp/ReportingTestWinApp/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.lblReportSelection = new System.Windows.Forms.Label();
+            this.cmbReports = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmbReports);
+            this.panel1.Controls.Add(this.lblReportSelection);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer.Location = new System.Drawing.Point(0, 30);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.Size = new System.Drawing.Size(696, 509);
+            this.reportViewer.TabIndex = 1;
+            // 
+            // lblReportSelection
+            // 
+            this.lblReportSelection.AutoSize = true;
+            this.lblReportSelection.Location = new System.Drawing.Point(6, 7);
+            this.lblReportSelection.Name = "lblReportSelection";
+            this.lblReportSelection.Size = new System.Drawing.Size(75, 13);
+            this.lblReportSelection.TabIndex = 0;
+            this.lblReportSelection.Text = "Select Report:";
+            // 
+            // cmbReports
+            // 
+            this.cmbReports.FormattingEnabled = true;
+            this.cmbReports.Location = new System.Drawing.Point(88, 4);
+            this.cmbReports.Name = "cmbReports";
+            this.cmbReports.Size = new System.Drawing.Size(244, 21);
+            this.cmbReports.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 539);
+            this.Controls.Add(this.reportViewer);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.Label lblReportSelection;
+        private System.Windows.Forms.ComboBox cmbReports;
     }
 }
 
--- ReportingApp/ReportingTestWinApp/Form1.cs
+++ ReportingApp/ReportingTestWinApp/Form1.cs
@@ -16,5 +16,11 @@ namespace ReportingTestWinApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer.RefreshReport();
+        }
     }
 }
